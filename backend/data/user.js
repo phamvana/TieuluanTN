de647b3aeb0aedde403921e33437cfcbcbe255f2
Pham Van A <phamvana@gmail.com>
2024-03-25 10:59:06 +0700

    save data

--- backend/data/user.js
+++ backend/data/user.js
@@ -15,3 +15,14 @@ const users = [{
 },];
 
 export default users;
+
+/**
+ * dùng postman đăng ký tài khoản mới
+ *  {
+    "_id": "6600f2f427c131aa20c6ff57",
+    "name": "phamvana",
+    "email": "pva@example.com",
+    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MDBmMmY0MjdjMTMxYWEyMGM2ZmY1NyIsImlhdCI6MTcxMTMzODIyOSwiZXhwIjoxNzEzOTMwMjI5fQ.QKH2hwpmZfFU6MaHiVMA_gmBXU-h3HPLJeOPb8t8wgw",
+    "isAdmin": false
+}
+ */
